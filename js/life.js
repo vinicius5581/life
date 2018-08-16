c51a142c395b45b905ba2d2764f75de614829a03
Vinicius Santana <viniciussantana@Viniciuss-MacBook-Pro.local>
2018-08-15 17:41:37 -0700

    refactoring to a class

--- js/life.js
+++ js/life.js
@@ -1,97 +1,112 @@
-var Board;
-var nextBoard;
-var xsize = 10;
-var ysize = 10;
-var dead = 0;
-var alive = 1;
+class Life {
+  constructor() {
+    this.Board = [];
+    this.boardSnap = [];
+    this.xsize = 20;
+    this.ysize = 20;
+    this.dead = 0;
+    this.alive = 1;
+    this.Neighbors = this.Neighbors.bind(this);
+    this.Kill = this.Kill.bind(this);
+    this.MakeLive = this.MakeLive.bind(this);
+    this.NextStep = this.NextStep.bind(this);
+  }
 
-function Neighbors(boardSnap, x, y) {
-  var n = 0;
-  for (dx = -1; dx < 1; ++dx)
-    for (dy = -1; dy < 1; ++dy) {
-      var ax = x + dx;
-      var ay = y + dy;
-      if (ax >= 0 && ax < xsize && ay >= 0 && ay < ysize) {
-        if (boardSnap[ax][ay] === alive) {
-          ++n;
+  Neighbors(boardSnap, x, y) {
+    var n = 0;
+    for (var dx = -1; dx < 1; ++dx)
+      for (var dy = -1; dy < 1; ++dy) {
+        var ax = x + dx;
+        var ay = y + dy;
+        if (ax >= 0 && ax < this.xsize && ay >= 0 && ay < this.ysize) {
+          console.log(`x: ${x} y: ${y} - ax: ${ax} ay: ${ay}`);
+          if (boardSnap[ax][ay] === this.alive) {
+            n++;
+          }
+        }
+      }
+    return n;
+  }
+
+  Kill(boardSnap, x, y) {
+    if (this.Board[x][y] === this.alive) this.Board[x][y] = this.dead;
+  }
+
+  MakeLive(boardSnap, x, y) {
+    if (this.Board[x][y] === this.dead) this.Board[x][y] = this.alive;
+  }
+
+  NextStep() {
+    this.boardSnap = [].concat(this.Board);
+    for (var x = 0; x < this.xsize; x++) {
+      for (var y = 0; y < this.ysize; y++) {
+        var n = this.Neighbors(this.boardSnap, x, y);
+        if (n === 3) {
+          this.MakeLive(this.Board, x, y);
+        }
+        if (n < 2 || n > 3) {
+          this.Kill(this.Board, x, y);
         }
       }
     }
-  return n;
-}
+    this.DrawBoard();
+  }
 
-function Kill(boardSnap, x, y) {
-  if (boardSnap[x][y] === alive) Board[x][y] = dead;
-}
+  DrawBoard() {
+    var Text = "";
+    for (var y = 0; y < this.ysize; ++y) {
+      for (var x = 0; x < this.xsize; ++x)
+        Text += this.Board[x][y] == this.alive ? "o" : "_";
+      Text += "<br/>";
+    }
+    document.getElementById("board").innerHTML = Text;
+  }
 
-function MakeLive(boardSnap, x, y) {
-  if (boardSnap[x][y] === dead) Board[x][y] = alive;
-}
+  Main() {
+    // *** Change this variable to choose a different baord setup from below
+    var BoardSetup = "flower";
 
-function NextStep(Board) {
-  var boardSnap = [].concat(Board);
-  for (var x = 0; x < xsize; x++) {
-    for (var y = 0; y < ysize; y++) {
-      var n = Neighbors(boardSnap, x, y);
-      if (n === 3) {
-        MakeLive(boardSnap, x, y);
-      }
-      if (n < 2 || n > 3) {
-        Kill(boardSnap, x, y);
+    this.Board = new Array(this.xsize);
+    for (var x = 0; x < this.xsize; ++x) {
+      this.Board[x] = new Array(this.ysize);
+      for (var y = 0; y < this.ysize; ++y) {
+        this.Board[x][y] = 0;
       }
     }
+
+    if (BoardSetup == "blinker") {
+      this.Board[1][0] = 1;
+      this.Board[1][1] = 1;
+      this.Board[1][2] = 1;
+    } else if (BoardSetup == "glider") {
+      this.Board[2][0] = 1;
+      this.Board[2][1] = 1;
+      this.Board[2][2] = 1;
+      this.Board[1][2] = 1;
+      this.Board[0][1] = 1;
+    } else if (BoardSetup == "flower") {
+      this.Board[4][6] = 1;
+      this.Board[5][6] = 1;
+      this.Board[6][6] = 1;
+      this.Board[7][6] = 1;
+      this.Board[8][6] = 1;
+      this.Board[9][6] = 1;
+      this.Board[10][6] = 1;
+      this.Board[4][7] = 1;
+      this.Board[6][7] = 1;
+      this.Board[8][7] = 1;
+      this.Board[10][7] = 1;
+      this.Board[4][8] = 1;
+      this.Board[5][8] = 1;
+      this.Board[6][8] = 1;
+      this.Board[7][8] = 1;
+      this.Board[8][8] = 1;
+      this.Board[9][8] = 1;
+      this.Board[10][8] = 1;
+    }
+
+    this.DrawBoard(this.Board);
   }
 }
 
-function DrawBoard(Board) {
-  var Text = "";
-  for (var y = 0; y < ysize; ++y) {
-    for (var x = 0; x < xsize; ++x) Text += Board[x][y] == alive ? "o" : "_";
-    Text += "<br/>";
-  }
-  document.getElementById("board").innerHTML = Text;
-}
-
-function Main() {
-  // *** Change this variable to choose a different baord setup from below
-  var BoardSetup = "blinker";
-
-  Board = new Array(xsize);
-  for (var x = 0; x < xsize; ++x) {
-    Board[x] = new Array(ysize);
-    for (var y = 0; y < ysize; ++y) Board[x][y] = 0;
-  }
-
-  if (BoardSetup == "blinker") {
-    Board[1][0] = 1;
-    Board[1][1] = 1;
-    Board[1][2] = 1;
-  } else if (BoardSetup == "glider") {
-    Board[2][0] = 1;
-    Board[2][1] = 1;
-    Board[2][2] = 1;
-    Board[1][2] = 1;
-    Board[0][1] = 1;
-  } else if (BoardSetup == "flower") {
-    Board[4][6] = 1;
-    Board[5][6] = 1;
-    Board[6][6] = 1;
-    Board[7][6] = 1;
-    Board[8][6] = 1;
-    Board[9][6] = 1;
-    Board[10][6] = 1;
-    Board[4][7] = 1;
-    Board[6][7] = 1;
-    Board[8][7] = 1;
-    Board[10][7] = 1;
-    Board[4][8] = 1;
-    Board[5][8] = 1;
-    Board[6][8] = 1;
-    Board[7][8] = 1;
-    Board[8][8] = 1;
-    Board[9][8] = 1;
-    Board[10][8] = 1;
-  }
-
-  DrawBoard(Board);
-}
+const life = new Life();
